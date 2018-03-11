476ed9834e19a4a5a86fc1706dd3baffa2494420
Swolebrain <thecodingteacher@gmail.com>
2018-03-11 00:47:16 -0500

    trying subdomain

--- src/globals.js
+++ src/globals.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? 'http://titanhack.com/kotlin/questions' : 'http://localhost:1338/questions';
+export const API_URL = process.env.NODE_ENV === 'production' ? 'http://kotlin.titanhack.com/questions' : 'http://localhost:1338/questions';
