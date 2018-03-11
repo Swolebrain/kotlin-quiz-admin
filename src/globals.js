c74a2c50b6d8fd5b2ac0c33cb7bbba355ff97660
Swolebrain <thecodingteacher@gmail.com>
2018-03-11 01:30:44 -0500

    changed back end to different subdomain

--- src/globals.js
+++ src/globals.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? 'http://kotlin.titanhack.com/kotlin/questions' : 'http://localhost:1338/kotlin/questions';
+export const API_URL = process.env.NODE_ENV === 'production' ? 'http://api.titanhack.com/kotlin/questions' : 'http://localhost:1338/kotlin/questions';
